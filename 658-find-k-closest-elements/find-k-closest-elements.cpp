9857543006d992c916ad3e3330bea17139d8b257
Ayush Agarwal <116223165+ayu5hagarwal@users.noreply.github.com>
2024-01-22 22:04:53 +0530

    Time: 25 ms (89.22%) | Memory: 33.6 MB (27.34%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,25 +1,47 @@
 class Solution {
 public:
-    vector<int> twoPointerMethod(vector<int>& arr,int k,int x){
-        int l=0,h=arr.size()-1;
-        while(h-l>=k){
-            if(x-arr[l]>arr[h]-x){
-                l++;
+    int lowerBound(vector<int>& arr,int x){
+        int s = 0;
+        int e = arr.size() -1;
+        int ans = e;
+        while(s<=e){
+            int mid = (s+e)/2;
+            if(arr[mid]>=x){
+                ans = mid;
+                e = mid-1;
+            }
+            else if(x>arr[mid]){
+                s =mid+1;
             }
             else{
-                h--;
+                e = mid - 1;
             }
         }
-        // vector<int> ans;
-        // for(int i = l;i<=h;i++){
-        //     ans.push_back(arr[i]);
-        // }
-        // return ans;
+        return ans;
+    }
 
-        return  vector<int>(arr.begin() + l,arr.begin() + h + 1);
+    vector<int> binarySearchMethod(vector<int>& arr,int k,int x){
+        //lower bound
+        int h = lowerBound(arr,x);
+        int l = h-1;
+        while(k--){
+            if(l<0){
+                h++;
+            }
+            else if(h>=arr.size()){
+                l--;
+            }
+            else if(x-arr[l]>arr[h]-x){
+                h++;
+            }
+            else{
+                l--; 
+            }
+        }
+        return vector<int>(arr.begin() + l + 1,arr.begin() + h);
     }
 
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        return twoPointerMethod(arr,k,x);
+        return binarySearchMethod(arr,k,x);
     }
 };
